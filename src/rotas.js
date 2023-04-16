320b3d5ccc3fbf2ada88156fff4d7a7d8b2397f2
William Diangellis <diangellis2014@gmail.com>
2023-04-16 20:17:54 -0300

    algumas correções e deploy 2

--- src/rotas.js
+++ src/rotas.js
@@ -3,5 +3,7 @@ const { listar, cadastrar } = require('./controlador')
 
 rotas.get('/usuarios', listar)
 rotas.post('/usuarios', cadastrar)
-rotas.get('/', (req, res) => res.send('Olá, mundo!'))
+rotas.get('/', (req, res) =>
+  res.send('Para cadastrar um usuario, enviar um post para a url/usuarios')
+)
 module.exports = rotas
